3e5fd398aea95a70b85c396b0954c4137823d802
Alex Langberg <alexlangberg@gmail.com>
2014-05-20 13:38:28 +0200

    Throw exception if command is too long.
    Otherwise it will simply file silently. This only implements on Windows.

--- src/Barryvdh/Queue/AsyncQueue.php
+++ src/Barryvdh/Queue/AsyncQueue.php
@@ -64,7 +64,7 @@ class AsyncQueue extends Queue implements QueueInterface {
         $command = sprintf($string, $payload, $environment);
 
         if (strlen($command) > 8191 && substr(php_uname(), 0, 7) == "Windows") {
-            throw new \Exception('Total async job command was more than 8191 characters which is not supported by Windows.');
+            throw new \Exception('Total async job command was more than 8191 characters which is not supported by Windows cmd.');
         }
 
         return new Process($command, $cwd);
